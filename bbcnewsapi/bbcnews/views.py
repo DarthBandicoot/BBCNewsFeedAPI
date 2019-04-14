e1cd143cd302ccfd08c45cf54973e1e117d1d36d
Lewis Collins <lew.collins94@gmail.com>
2019-04-14 16:54:58 +0100

    work on first view and function to get news articles

--- bbcnewsapi/bbcnews/views.py
+++ bbcnewsapi/bbcnews/views.py
@@ -1,10 +1,26 @@
+import feedparser
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 
-class NewsList(TemplateView):
+class NewsList(ListView):
+    template_name = ''
+    context_object_name = ''
+    url = 'http://feeds.bbci.co.uk/news/world/rss.xml'
+    results = []
 
-    def requests(self):
-        bbc = 'http://feeds.bbci.co.uk/news/world/rss.xml'
+    def get_context_data(self, request, *args, **kwargs):
+        context = super(request, *args, **kwargs)
+
+        return context
+
+    def news_results(self):
+        bbcFeed = feedparser.parse('http://feeds.bbci.co.uk/news/world/rss.xml')
+        news_articles = bbcFeed.entries[:10]
+
+        for article in news_articles:
+
+            self.results.append(article)
+        return self.results
--- bbcnewsapi/scripts/xmlparse.py
+++ bbcnewsapi/scripts/xmlparse.py
@@ -3,9 +3,12 @@ import feedparser
 
 def rssfeedparser():
     bbcFeed = feedparser.parse('http://feeds.bbci.co.uk/news/world/rss.xml')
-    entry = bbcFeed.entries[1]
     entries = []
-    entries.append(entry.values)
+    news_articles = bbcFeed.entries[:10]
+
+    for article in news_articles:
+        print(article)
+        entries.append(article)
 
     return entries
 
